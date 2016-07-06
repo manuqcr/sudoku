b84536751707988f442ef2dc8dfd99e5078bf783
emmanuel <emmanuel>
2016-07-06 23:41:25 +0200

    On fait une grille avec 81 cases

--- src/MainDialog.java
+++ src/MainDialog.java
@@ -1,14 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class MainDialog extends JDialog {
     private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
+    GridLayout gridManager = new GridLayout(9,9);
+
+    ArrayList<JButton> buttons = new ArrayList<JButton>(81);
 
     public MainDialog() {
+
         setContentPane(contentPane);
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
+
+        setLayout(gridManager);
+        generateGrid();
+
+        getButton(0,5).setBackground(Color.BLACK);
+    }
+
+    JButton getButton(int row, int column){
+        return buttons.get(row*9+column);
+    }
+
+    private void generateGrid() {
+        for (int i = 0; i < 9; ++i){
+            for (int j = 0; j < 9; ++j) {
+                JButton button = new JButton(i + " " + j);
+                buttons.add(button);
+                this.contentPane.add(button);
+            }
+        }
     }
 
     public static void main(String[] args) {
